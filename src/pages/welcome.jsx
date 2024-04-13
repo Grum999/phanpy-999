d54315287ca3b632a3abebcab6ef62a45e6658fc
Grum 999 <grum999@grum.fr>
2024-04-13 09:42:50 +0200

    Update G999

--- src/pages/welcome.jsx
+++ src/pages/welcome.jsx
@@ -46,7 +46,7 @@ function Welcome() {
             />
             <img src={logoText} alt="Phanpy" width="200" />
           </h1>
-          <p class="desc">A minimalistic opinionated Mastodon web client.</p>
+          <p class="desc">Phanpy-999</p>
           <p>
             <Link
               to={
@@ -84,83 +84,14 @@ function Welcome() {
           </p>
         )}
         <p>
-          <a href="https://github.com/cheeaun/phanpy" target="_blank">
-            Built
-          </a>{' '}
-          by{' '}
-          <a
-            href="https://mastodon.social/@cheeaun"
-            target="_blank"
-            onClick={(e) => {
-              e.preventDefault();
-              states.showAccount = 'cheeaun@mastodon.social';
-            }}
-          >
-            @cheeaun
-          </a>
-          .{' '}
-          <a href={PRIVACY_POLICY_URL} target="_blank">
-            Privacy Policy
-          </a>
-          .
+          <big>
+            <b>
+              <Link to="/login" class="button">
+                Log in
+              </Link>
+            </b>
+          </big>
         </p>
-      </div>
-      <div id="why-container">
-        <div class="sections">
-          <section>
-            <img
-              src={boostsCarouselUrl}
-              alt="Screenshot of Boosts Carousel"
-              loading="lazy"
-            />
-            <h4>Boosts Carousel</h4>
-            <p>
-              Visually separate original posts and re-shared posts (boosted
-              posts).
-            </p>
-          </section>
-          <section>
-            <img
-              src={nestedCommentsThreadUrl}
-              alt="Screenshot of nested comments thread"
-              loading="lazy"
-            />
-            <h4>Nested comments thread</h4>
-            <p>Effortlessly follow conversations. Semi-collapsible replies.</p>
-          </section>
-          <section>
-            <img
-              src={groupedNotificationsUrl}
-              alt="Screenshot of grouped notifications"
-              loading="lazy"
-            />
-            <h4>Grouped notifications</h4>
-            <p>
-              Similar notifications are grouped and collapsed to reduce clutter.
-            </p>
-          </section>
-          <section>
-            <img
-              src={multiColumnUrl}
-              alt="Screenshot of multi-column UI"
-              loading="lazy"
-            />
-            <h4>Single or multi-column</h4>
-            <p>
-              By default, single column for zen-mode seekers. Configurable
-              multi-column for power users.
-            </p>
-          </section>
-          <section>
-            <img
-              src={multiHashtagTimelineUrl}
-              alt="Screenshot of multi-hashtag timeline with a form to add more hashtags"
-              loading="lazy"
-            />
-            <h4>Multi-hashtag timeline</h4>
-            <p>Up to 5 hashtags combined into a single timeline.</p>
-          </section>
-        </div>
       </div>
     </main>
   );
